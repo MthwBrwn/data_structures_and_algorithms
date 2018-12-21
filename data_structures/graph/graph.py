ad29e27a9775ac070adc2ace8a5ee7a8d00a68b1
mthwbrwn <mthwbrwn@gmail.com>
2018-12-21 14:49:57 -0800

    initial set up complete

---
+++ data_structures/graph/graph.py
@@ -0,0 +1,43 @@
+class Graph(object):
+    """this class creates a graph object. This object has the ability to have a node with a value
+    nd a number of edges.
+    """
+    def __init__(self, ):
+        self.graph = {}
+
+    def __repr__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    def __len__(self):
+        pass
+
+    def has_vert(self, val):
+        """
+        """
+        # checks for a key in the graph
+        pass
+
+    def add_vert(self, val):
+        """
+        """
+        # add vertice to self.graph
+        # check to see if the vert already exists: if so raise exception
+            # create a helper method
+        pass
+
+
+    def add_edge(self, v1, v2, weight):
+        """
+        """
+        # add a relationship and weight between two verts
+        # don't forget to validate
+        pass
+
+    def get_neighbors(self, val):
+        """
+        """
+        # Given a val (key), return all adjacent verts
+        pass
