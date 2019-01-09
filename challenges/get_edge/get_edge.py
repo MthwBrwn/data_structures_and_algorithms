86001486b1eaeac9aa2b18193a26ee8d45b53271
mthwbrwn <mthwbrwn@gmail.com>
2019-01-08 23:18:17 -0800

    unable to complete exercise

---
+++ challenges/get_edge/get_edge.py
@@ -0,0 +1,11 @@
+
+# need to take elements of the list from input and determine if the graph has direct flights
+
+def get_edge(given_list, given_graph):
+    """
+    """
+
+
+# need to iterate through the given list obtaining current value and the next value in the list
+
+# need to run a has_neighbor method to see if current_next is dictionary item
