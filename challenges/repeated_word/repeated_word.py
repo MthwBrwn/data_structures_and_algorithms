1b9054ada458616de23e614d2bfd9a3753d3780c
mthwbrwn <mthwbrwn@gmail.com>
2019-01-15 14:18:28 -0800

    split does not work

--- challenges/repeated_word/repeated_word.py
+++ challenges/repeated_word/repeated_word.py
@@ -3,21 +3,27 @@ def find_first_repeat(string):
     """ This function finds the first word that is repeated in a string
     and returns the first repeated word.
     """
-    bucket = []
     size = 64
-    split_list = [get_words_from_string(string)]
+    bucket = [[None]* size]
 
-    for i in range(len(split_list)):
-        hash = sum_hash(split_list[i])
-        if bucket[hash] == (split_list[i], 1):
-            return split_list[i]
-        else:
-            bucket[hash] = (split_list[i], 1)
+
+    # if type(input_string) is not str:
+    #     raise TypeError(f'You will need to submit a string')
+
+    # for i in range(len(split_list)):
+    #     hash = sum_hash(split_list[i])
+    #     if bucket[hash] == (split_list[i], 1):
+    #         return split_list[i]
+    #     else:
+    #         bucket[hash] = (split_list[i], 1)
 
 # get word from string
     split_list = get_words_from_string(string)
+
+    return split_list
+
 # hash key
-    sum_hash(key)
+    # sum_hash(key)
 
 #check value - if value is 1 , return key
 
@@ -33,10 +39,11 @@ def sum_hash(key):
     return hash % size
 
 
-def get_words_from_string(input):
+def get_words_from_string(input_):
+    """This splits a string into individual words separated by empty spaces or punctuation
+
     """
-    """
-    return (‘’.join((char if char.isalpha() else ” “) for char in input).split())
+    return (‘’.join((char if char.isalpha() else ” “) for char in input_).split())
 
 
 
--- challenges/repeated_word/test_repeat.py
+++ challenges/repeated_word/test_repeat.py
@@ -1,4 +1,13 @@
-import repeated_word
+from repeated_word import find_first_repeat, get_words_from_string
 import pytest
 
-assert find_first_repeat
+def test_existence():
+    """ This test checks to see if import works.
+    """
+    assert find_first_repeat
+
+def test_component_of_repeat():
+    """
+    """
+    test = find_first_repeat('try this one')
+    assert test == ('try', 'this', 'one')
