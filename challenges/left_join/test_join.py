879e200e185aa4abc90d61afdd74fc527aa4609b
mthwbrwn <mthwbrwn@gmail.com>
2019-01-16 13:59:52 -0800

    testing started

---
+++ challenges/left_join/test_join.py
@@ -0,0 +1,10 @@
+from left_join import left_join
+
+
+def test_hash_join():
+    """
+    """
+    hashA = {'red': 1, 'orange': 2, 'yellow': 3 }
+    hashB = {'red': 4, 'orange': 5, 'yellow': 6 }
+    testAB = hash_join(hashA, hashB)
+    assert testAB = [['red', 1, 4], ['orange', 2, 5],['yellow', 3, 6]]
