ec7fa060dc0aa15a628ffaae2316c85f2943e892
mthwbrwn <mthwbrwn@gmail.com>
2019-01-16 14:08:54 -0800

    pseudoadded

---
+++ challenges/left_join/left_join.py
@@ -0,0 +1,12 @@
+
+#  create a list to store data - list_join
+
+def left_join( hashmapA, hashmapB):
+    """
+    """
+# for all items in hashmapA push hashmap[0] into list_join
+
+# for length of list join get hashmapB[listjoin[i]] and push to list_join
+# if no value push "null"
+#
+# return list_join
--- challenges/left_join/test_join.py
+++ challenges/left_join/test_join.py
@@ -4,7 +4,7 @@ from left_join import left_join
 def test_hash_join():
     """
     """
-    hashA = {'red': 1, 'orange': 2, 'yellow': 3 }
+    hashA = {'red': 1, 'orange': 2, 'yellow': 3}
     hashB = {'red': 4, 'orange': 5, 'yellow': 6 }
-    testAB = hash_join(hashA, hashB)
-    assert testAB = [['red', 1, 4], ['orange', 2, 5],['yellow', 3, 6]]
+    testAB = left_join(hashA, hashB)
+    assert testAB == [['red', 1, 4], ['orange', 2, 5], ['yellow', 3, 6]]
