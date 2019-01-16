ba58b405010a0205bc4ee357ae19b5ed97ec1d6b
mthwbrwn <mthwbrwn@gmail.com>
2019-01-15 21:17:21 -0800

    set up algorithm

---
+++ challenges/tree_intersection/tree_intersection.py
@@ -0,0 +1,5 @@
+
+# function will need to take in two values
+# traverse tree #1, hashing each node value and placing into a list_1 (hash table)
+# traverse tree #2, at each node, if hashed value of node is in list_1, then  value of node is placed in list_2
+#  return list_2
