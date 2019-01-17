ce6709ff2f1412bf914d3f9c52dafe7b0cc315b1
mthwbrwn <mthwbrwn@gmail.com>
2019-01-16 21:42:52 -0800

    did some reading refactored function - initial test passes

--- challenges/left_join/left_join.py
+++ challenges/left_join/left_join.py
@@ -1,12 +1,24 @@
 
+
+def left_join(hashmapA, hashmapB):
+    """ This function takes two hashmaps and inserts the data for the 2nd and adds to the 1st
+
+    """
 #  create a list to store data - list_join
-
-def left_join( hashmapA, hashmapB):
-    """
-    """
-# for all items in hashmapA push hashmap[0] into list_join
-
-# for length of list join get hashmapB[listjoin[i]] and push to list_join
-# if no value push "null"
+    list_join = []
+# for all items in hashmapA append hashmap[0] into list_join
+    for item in hashmapA:
+        list_join.append(item)
+        list_join.append(hashmapA.get(item))
+        list_join.append(hashmapB.get(item))
+# [hashmapA[item]]
+# for length of list join get hashmapB[listjoin[i]] and append to list_join
+# if no value append "null"
+    # for i in range(len(list_join)):
+    #     if hashmapB[list_join[i]][1]:
+    #         list_join[i].append(hashmapB[list_join[i]][1])
+    #     else:
+    #         list_join[i] = 'null'
 #
 # return list_join
+    return list_join
--- challenges/left_join/test_join.py
+++ challenges/left_join/test_join.py
@@ -4,7 +4,7 @@ from left_join import left_join
 def test_hash_join():
     """
     """
-    hashA = {'red': 1, 'orange': 2, 'yellow': 3}
-    hashB = {'red': 4, 'orange': 5, 'yellow': 6 }
-    testAB = left_join(hashA, hashB)
-    assert testAB == [['red', 1, 4], ['orange', 2, 5], ['yellow', 3, 6]]
+    hashmapA = {'red': 1, 'orange': 2, 'yellow': 3}
+    hashmapB = {'red': 4, 'orange': 5, 'yellow': 6}
+    testAB = left_join(hashmapA, hashmapB)
+    assert testAB == ['red', 1, 4, 'orange', 2, 5, 'yellow', 3, 6]
