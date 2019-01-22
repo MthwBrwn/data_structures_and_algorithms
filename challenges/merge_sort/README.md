892fe480fab85216f1dcc649ffbae28cdd80bfa9
mthwbrwn <mthwbrwn@gmail.com>
2019-01-21 23:55:03 -0800

    have problems with testing needs work

--- challenges/merge_sort/README.md
+++ challenges/merge_sort/README.md
@@ -8,7 +8,8 @@ Write a function that accepts an array of unsorted integers, and returns a sorte
 
 ## Approach & Efficiency
 
-
+This function does make use of a list while operating and the space will be proportional to the size of the list so space complexity is O(n)
+Time complexity for this oeration is O(n log n ) -
 
 ## Solution
 
--- challenges/merge_sort/merge_sort.py
+++ challenges/merge_sort/merge_sort.py
@@ -1,13 +1,15 @@
+
 def mergesort(_list):
     """ merge sort works by recursively spliting a list down to a
     binary comparison and then sorting smaller values to the left
     """
+
 # Need a recursive function that splits a list
 # in half until a list of length n has n lists of 1 unit
-    if len(_list)>1:
-        half = len(_list//2)
-        left_half = _list[half:]
-        right_half = _list[:half]
+    if len(_list) > 1:
+        half = len(_list)//2
+        left_half = _list[:half]
+        right_half = _list[half:]
 
         mergesort(left_half)
         mergesort(right_half)
@@ -17,26 +19,26 @@ def mergesort(_list):
 # we need to keep track of three counters
     i = 0  # index of left side
     j = 0  # index of right side
-    k = 0 # index of _list
+    k = 0  # index of _list
 
     while i < len(left_half) and j < len(right_half):
             if left_half[i] < right_half[j]:
-                _list[k]=left_half[i]
-                i=i+1
+                _list[k] = left_half[i]
+                i += i
             else:
-                _list[k]=righthalf[j]
-                j=j+1
-            k=k+1
+                _list[k] = right_half[j]
+                j += j
+            k += k
 
 # when left or right is no longer present the following continue the sort
     while i < len(left_half):
-            _list[k]=left_half[i]
-            i=i+1
-            k=k+1
+            _list[k] = left_half[i]
+            i += i
+            k += k
 
     while i < len(right_half):
-            _list[k]=right_half[i]
-            i=i+1
-            k=k+1
+            _list[k] = right_half[j]
+            j += j
+            k += k
 
     return _list
