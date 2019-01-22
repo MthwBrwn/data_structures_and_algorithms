56eb111824a6a4f40839945b6930fdefef693d93
mthwbrwn <mthwbrwn@gmail.com>
2019-01-21 23:00:18 -0800

    white board and readme added pseudo put down

---
+++ challenges/merge_sort/README.md
@@ -0,0 +1,15 @@
+# Mergesort
+
+Implement Mergesort
+
+## Challenge
+
+Write a function that accepts an array of unsorted integers, and returns a sorted array by a recursive mergesort algorithm.
+
+## Approach & Efficiency
+
+
+
+## Solution
+
+![](../../assets/merge_sort.jpg)
---
+++ challenges/merge_sort/merge_sort.py
@@ -0,0 +1,5 @@
+# Need a recursive function that splits a list
+# in half until a list of length n has n lists of 1 unit
+
+# once down to the last split, the left list is compared to the right list and the lower value is appended to newList.
+# this runs recursively until the list is fully rebuilt
